0b202ad3d3057d493385ffbc070f6bb5e39b7951
Quentin Crida <qcrida@gmail.com>
2019-11-12 21:14:47 +0000

    bear name and type tested

--- bear.rb
+++ bear.rb
@@ -1,1 +1,21 @@
-# this will nave bear class
+class Bear
+
+  attr_reader :name, :type, :stomach
+
+  def initialize(name, type, stomach)
+    @name = name
+    @type = type
+    @stomach = []
+
+  end
+
+
+  def bear_name()
+    @name = name
+  end
+
+  def bear_type()
+    @type = type
+  end
+#
+end
--- spec/bear_spec.rb
+++ spec/bear_spec.rb
@@ -4,7 +4,19 @@ require_relative("../river.rb")
 require_relative("../bear.rb")
 require_relative("../fish.rb")
 
-class RiverTest < MiniTest::Test
+class BearTest < MiniTest::Test
+
+  def setup()
+    @bear = Bear.new("Bill", "Grizzly", [])
+  end
+
+  def test_bear_name()
+    assert_equal("Bill", @bear.name)
+  end
+
+  def test_bear_type()
+    assert_equal("Grizzly", @bear.type)
+  end
 
 
 
