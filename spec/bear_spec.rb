810ecd98c1b1afe06d1f42c77cbecfc71d0e846d
Quentin Crida <qcrida@gmail.com>
2019-11-12 22:11:46 +0000

    bear method namechanges

--- spec/bear_spec.rb
+++ spec/bear_spec.rb
@@ -7,19 +7,19 @@ require_relative("../fish.rb")
 class BearTest < MiniTest::Test
 
   def setup()
-    @test_bear = Bear.new("Bill", "Grizzly", [])
+    @bear = Bear.new("Bill", "Grizzly", [])
   end
 
   def test_bear_name()
-    assert_equal("Bill", @test_bear.name)
+    assert_equal("Bill", @bear.name)
   end
 
   def test_bear_type()
-    assert_equal("Grizzly", @test_bear.type)
+    assert_equal("Grizzly", @bear.type)
   end
 
   def test_bear_stomach()
-    assert_equal([], @test_bear.stomach)
+    assert_equal([], @bear.stomach)
   end
 
 
